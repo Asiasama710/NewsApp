380cebbb81f989fe3fdbc1a2c59ad14e35d3cd3c
Asia sama <sehunexo710@gmail.com>
2024-04-16 02:32:55 +0300

    add colors

--- app/src/main/java/com/asia/newsapp/ui/theme/Color.kt
+++ app/src/main/java/com/asia/newsapp/ui/theme/Color.kt
@@ -5,44 +5,44 @@ import androidx.compose.ui.graphics.Color
 val TernaryShadesDark = Color(0x6104030A)
 val QuaternaryShadesDark = Color(0x2904030A)
 val BackgroundLight = Color(0xFFF6F7F7)
-val SecondaryLight = Color(0xFFDDEDFF)
+val SecondaryLight = Color(0xFFF8CF54)
 
 
 data class Colors(
     val primary: Color,
-    val contentPrimary: Color,
     val secondary : Color,
+    val contentPrimary: Color,
     val onSecondary: Color,
     val contentTertiary: Color,
     val surface: Color,
     val onPrimary: Color,
     val background: Color,
-    val green: Color,
+    val border: Color,
     val onSurface: Color,
 )
 
 val LightColors = Colors(
-        primary = Color(0xFF585896),
-        secondary = SecondaryLight,
+        primary = Color(0xFFE5AD06),
+        secondary = Color(0xFFF8CF54),
         onSecondary = Color(0xDEFFFFFF),
-        contentPrimary = Color(0xDE000A1F),
-        contentTertiary = Color(0x61000B1F),
+        contentPrimary = Color(0xFF222222),
+        contentTertiary = Color(0xFF6D6D6D),
         surface = QuaternaryShadesDark,
-        background = Color(0xFFFAFAFA),
-        green = Color(0xFF31B43B),
-        onPrimary = Color(0xFFECF2FF),
         onSurface = TernaryShadesDark,
+        background = Color(0xFFFAFAFA),
+        border = Color(0xFFE6E6E6),
+        onPrimary = Color(0xFFFFFFFF),
 )
 
 val DarkColors = Colors(
-        primary = Color(0xFF42426F),
-        secondary = SecondaryLight,
+        primary = Color(0xFFE5AD06),
+        secondary =  Color(0xFFF8CF54),
         onSecondary = Color(0xDEFFFFFF),
-        contentPrimary = Color(0xDEFFFFFF),
-        contentTertiary = Color(0x61FFFFFF),
+        contentPrimary = Color(0xFF222222),
+        contentTertiary = Color(0xFF6D6D6D),
         surface = QuaternaryShadesDark,
-        background = Color(0xFF1E1F24),
-        green = Color(0xFF31B43B),
-        onPrimary = Color(0xFFECF2FF),
         onSurface = TernaryShadesDark,
+        background = Color(0xFF1E1F24),
+        border = Color(0xFFE6E6E6),
+        onPrimary = Color(0xFFFFFFFF),
 )
